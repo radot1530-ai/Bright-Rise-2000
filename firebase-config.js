f15800fbdbd283d115ab628bfdd0d74cd07f4fc9
radot1530-ai <radot1530@gmail.com>
2026-03-15 11:00:58 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAgvH0CpF6tGISpfLw3JWJCT2beBG28wAM",
+  authDomain: "kaylakay-cdf64.firebaseapp.com",
+  databaseURL: "https://kaylakay-cdf64-default-rtdb.firebaseio.com/",
+  projectId: "kaylakay-cdf64",
+  storageBucket: "kaylakay-cdf64.appspot.com",
+  messagingSenderId: "663099511740",
+  appId: "1:663099511740:web:aeb6bddccee9666ff791b9"
+};
+
+firebase.initializeApp(firebaseConfig);
